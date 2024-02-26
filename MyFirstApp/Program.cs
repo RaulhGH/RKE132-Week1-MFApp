624803c81683855b906248dcfbd4b6b580d8bc56
Raulh <raulhaabu@gmail.com>
2024-02-26 15:25:11 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// Comment
+
+//rakendus küsib nime
+// rak tervitab nimepidi
+
+Console.WriteLine("Enter your name");
+//loob sõne
+string userName = Console.ReadLine();
+Console.WriteLine("Hello "+userName);
+
+
+//string intrerpolation
+Console.WriteLine($"Hello, {userName} !");
